157fa71eff30e3e63c5bc7acc3d72b1664c5ca0f
clwx-31 <309938308+clwx-31@users.noreply.github.com>
2026-08-25 09:09:53 -0500

    Add SAT Math surface-area shapes

--- scripts/generate-sat-math.js
+++ scripts/generate-sat-math.js
@@ -4668,6 +4668,7 @@ defineShapes({
 /* ------------------------ Geometry and Trigonometry --------------------- */
 
 importActShapeSubskill("area", "area");
+importActShapeSubskill("surface area", "surface area");
 
 // ---------------------------------------------------------------------------
 // Driver
